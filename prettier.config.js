6ff22325db3385c96de305c02218be34676f24fe
Jens Tschirpig <jens1101@users.noreply.github.com>
2024-11-03 13:30:28 +0200

    fix: Updated formatter
    - Added Prettier plugins to sort imports and "package.json"
    - Updated ".prettierignore"

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+const config = {
+  importOrderSortSpecifiers: true,
+  plugins: [
+    "prettier-plugin-packagejson",
+    "@trivago/prettier-plugin-sort-imports",
+  ],
+};
+
+export default config;
