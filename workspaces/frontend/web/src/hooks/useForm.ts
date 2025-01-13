876880d515cf1189f2e262a69d30abc47ce52db2
Jens Tschirpig <jens1101@users.noreply.github.com>
2025-01-13 12:38:57 +0200

    chore(#30): Removed unused import

--- workspaces/frontend/web/src/hooks/useForm.ts
+++ workspaces/frontend/web/src/hooks/useForm.ts
@@ -1,5 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import type { useFormField } from "./useFormField.js";
 import { type Accessor, createSignal, onCleanup } from "solid-js";
 
 /**
