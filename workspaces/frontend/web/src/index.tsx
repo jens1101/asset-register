c363d3c2e7d5e060843060632e07437b89ea9981
Jens Tschirpig <jens1101@users.noreply.github.com>
2025-01-13 10:26:56 +0200

    fix(#30): Made app bootstrapping effectful

--- workspaces/frontend/web/src/index.tsx
+++ workspaces/frontend/web/src/index.tsx
@@ -1,15 +1,36 @@
 import { App } from "./app.js";
+import { Modal } from "./components/Modal/Modal.jsx";
+import { ModalProvider } from "./context/modal.jsx";
 import { routes } from "./routes.js";
 import "./styles/index.scss";
 import { Router } from "@solidjs/router";
+import { Effect, Exit, Option, pipe } from "effect";
 import { render } from "solid-js/web";
 
-const root = document.getElementById("root");
+const program = pipe(
+  Effect.gen(function* () {
+    const root = yield* Option.fromNullable(document.getElementById("root"));
 
-if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
-  throw new Error(
-    "Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?",
-  );
-}
+    render(
+      () => (
+        <ModalProvider>
+          <Router root={App}>{routes}</Router>
+          <Modal />
+        </ModalProvider>
+      ),
+      root,
+    );
+  }),
+  Effect.mapError(
+    () =>
+      new Error(
+        "Root element not found. Did you forget to add it to your " +
+          "index.html? Or maybe the id attribute got misspelled?",
+      ),
+  ),
+);
 
-if (root) render(() => <Router root={App}>{routes}</Router>, root);
+// TODO: error handling
+void Effect.runPromiseExit(program).then((exit) => {
+  if (Exit.isFailure(exit) && import.meta.env.DEV) console.error(exit.cause);
+});
