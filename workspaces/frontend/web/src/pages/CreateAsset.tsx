a39991163512c66c1f760853426cf9b50bba4422
Jens Tschirpig <jens1101@users.noreply.github.com>
2025-01-14 09:56:19 +0200

    feat(#30): Refactored the create asset screen
    - Made better use of Effect.
    - Navigate to the asset view after creation.

--- workspaces/frontend/web/src/pages/CreateAsset.tsx
+++ workspaces/frontend/web/src/pages/CreateAsset.tsx
@@ -1,62 +1,62 @@
 import { AssetForm } from "../components/AssetForm/AssetForm.jsx";
 import { client } from "../gql-client/client.js";
 import {
+  type AssetFragment,
   CreateAssetDocument,
-  type CreateAssetInput,
   type CreateAssetMutation,
   type CreateAssetMutationVariables,
 } from "../gql-client/types/graphql.js";
 import type { AssetFormValues } from "../schemas/AssetFormValues.js";
 import { CreateAssetInputFromAssetFormValues } from "../schemas/CreateAssetInput.js";
-import { Schema } from "effect";
-import {
-  type Component,
-  createEffect,
-  createResource,
-  createSignal,
-  createUniqueId,
-} from "solid-js";
+import { useNavigate } from "@solidjs/router";
+import { Effect, Exit, Schema } from "effect";
+import { type Component, createUniqueId } from "solid-js";
 
-// TODO: create `EditAsset` page
+function createAsset(
+  formValues: typeof AssetFormValues.Type,
+): Effect.Effect<AssetFragment, Error> {
+  return Effect.gen(function* () {
+    const createAssetInput = yield* Schema.decode(
+      CreateAssetInputFromAssetFormValues,
+    )(formValues);
+
+    const { data, error } = yield* Effect.promise(() =>
+      client.mutation<CreateAssetMutation, CreateAssetMutationVariables>(
+        CreateAssetDocument,
+        {
+          data: createAssetInput,
+        },
+      ),
+    );
+
+    // TODO: Improve error handling
+    if (error) {
+      return yield* Effect.fail(error);
+    }
+
+    if (data?.createAsset.__typename === "Asset") {
+      return data.createAsset;
+    }
+
+    return yield* Effect.fail(new Error("Failed to create asset"));
+  });
+}
+
 export const CreateAsset: Component = () => {
   const formId = createUniqueId();
-
-  const [assetInput, setAssetInput] = createSignal<CreateAssetInput>();
-
-  const [asset] = createResource(assetInput, async (data: CreateAssetInput) => {
-    const { data: result, error } = await client.mutation<
-      CreateAssetMutation,
-      CreateAssetMutationVariables
-    >(CreateAssetDocument, {
-      data,
-    });
-
-    if (error) {
-      console.error(error);
-    }
-
-    // TODO: defect handling
-    return result?.createAsset;
-  });
+  const navigate = useNavigate();
 
   const onSubmit = async (formValues: typeof AssetFormValues.Type) => {
-    try {
-      setAssetInput(
-        await Schema.decodePromise(CreateAssetInputFromAssetFormValues)(
-          formValues,
-        ),
-      );
-    } catch (error) {
-      // TODO: proper error handling
-      console.log(error);
-    }
+    Exit.match(await Effect.runPromiseExit(createAsset(formValues)), {
+      onSuccess(asset) {
+        navigate(`/asset/${asset.id}`);
+      },
+      onFailure(cause) {
+        // TODO: error handling
+        console.error(cause);
+      },
+    });
   };
-
-  createEffect(() => {
-    // TODO: Handle errors and defects.
-    // TODO: On success show a message to the user and give him the option to add more or return home.
-    console.log(asset());
-  });
 
   return (
     <section class="container">
