02e6d9673caa15287d02f6d56207991bdfb0bb1f
Jens Tschirpig <jens1101@users.noreply.github.com>
2025-03-24 18:16:28 +0200

    feat(#55): Added basic spinner to home page

--- workspaces/frontend/web/src/pages/Home.tsx
+++ workspaces/frontend/web/src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { AssetListItem } from "../components/AssetListItem/AssetListItem.tsx";
+import { Spinner } from "../components/Spinner.tsx";
 import type { AssetListResource } from "../data/assetList.ts";
 import { Option, pipe } from "effect";
 import { type Component, For, Show, Suspense } from "solid-js";
@@ -13,7 +14,13 @@ export const Home: Component<{ data: AssetListResource }> = (props) => {
 
   return (
     <section class={"container"}>
-      <Suspense fallback={<span>...</span>}>
+      <Suspense
+        fallback={
+          <div class="d-flex justify-content-center">
+            <Spinner />
+          </div>
+        }
+      >
         <Show when={Option.getOrNull(assets())} keyed>
           {(assets) => (
             <div class={"row row-cols-1 row-cols-sm-2 row-cols-md-3"}>
